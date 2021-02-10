16deb3dc52e4f89fcd3bb08ad73ef946cb4eeee2
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2021-02-11 02:34:04 +0800

    Added scripts to convert gif to video.

---
+++ gif2video.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ $# -lt 2 ]; then
+    echo './gif2video.sh {input_filename} {output_filename} {fps}'
+    exit
+fi
+
+fn_input=$1
+fn_output=$2
+fps=$3
+dirname='/tmp/gif2video'
+rm -rf $dirname
+mkdir $dirname
+convert -coalesce $fn_input "$dirname/%05d.png"
+ffmpeg -r $fps -loop 1 -t 3 -i "$dirname/%05d.png" $fn_output
