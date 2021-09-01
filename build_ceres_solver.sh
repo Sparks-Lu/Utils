3aa0fc2b1fc720ca1b01403ac44ba705fb92bbef
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2021-09-01 15:17:08 +0800

    Added ceres solver build script. Work on Ubuntu18.04.

---
+++ build_ceres_solver.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# git clone https://ceres-solver.googlesource.com/ceres-solver && cd ceres-solver
+# CMake
+sudo apt-get install cmake
+# google-glog + gflags
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+# BLAS & LAPACK
+sudo apt-get install libatlas-base-dev
+# Eigen3
+sudo apt-get install libeigen3-dev
+# SuiteSparse and CXSparse (optional)
+sudo apt-get install libsuitesparse-dev
+
+rm -rf built && mkdir built && cd built && cmake ../ -DMINIGLOG=True && make && sudo make install
+# Optionally install Ceres, it can also be exported using CMake which
+# allows Ceres to be used without requiring installation, see the documentation
+# for the EXPORT_BUILD_DIR option for more information.
