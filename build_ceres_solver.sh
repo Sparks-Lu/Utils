12011aab2716eab2c37a663c5e9c40c625f34aa0
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2021-09-24 10:39:55 +0800

    Updated build script.

--- build_ceres_solver.sh
+++ build_ceres_solver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # git clone https://ceres-solver.googlesource.com/ceres-solver && cd ceres-solver
+git checkout 1.14.0
 # CMake
 sudo apt-get install cmake
 # google-glog + gflags
@@ -11,7 +12,7 @@ sudo apt-get install libeigen3-dev
 # SuiteSparse and CXSparse (optional)
 sudo apt-get install libsuitesparse-dev
 
-rm -rf built && mkdir built && cd built && cmake ../ -DMINIGLOG=True && make && sudo make install
+mkdir -p built && cd built && cmake ../ -DMINIGLOG=True && make && sudo make install
 # Optionally install Ceres, it can also be exported using CMake which
 # allows Ceres to be used without requiring installation, see the documentation
 # for the EXPORT_BUILD_DIR option for more information.
