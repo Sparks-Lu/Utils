6fa263eddb6c17e82a29bd28e9566dcc671cabe4
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2021-10-11 18:54:36 +0800

    Changed output file names to pad with 0 to make sorting correct.

--- rename_files_ordered.py
+++ rename_files_ordered.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 import shutil
 
 
@@ -7,9 +6,11 @@ def rename_files_ordered(path):
     if not os.path.isdir(path):
         raise Exception('Invalid folder name: {}'.format(path))
     fns = os.listdir(path)
+    id_str_width = len(str(len(fns))) + 1
     for i, fn in enumerate(fns):
         fn_in = os.path.join(path, fn)
-        fn_tmp = '{}{}'.format(i + 1, os.path.splitext(fn)[-1])
+        fn_tmp = '{}{}'.format(str(i + 1).zfill(id_str_width),
+                               os.path.splitext(fn)[-1])
         fn_out = os.path.join(path, fn_tmp)
         shutil.move(fn_in, fn_out)
         print('Renamed {} to {}'.format(fn_in, fn_out))
