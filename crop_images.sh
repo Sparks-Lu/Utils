84914be2ee6918422506461884f37085fe1a3d48
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2022-05-17 22:36:00 +0800

    Added shell scripts to crop and resize images.

---
+++ crop_images.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+files=`ls $1/*.JPG`
+echo "Files to be processed: $files"
+cnt=1
+for fn_in in $files; do
+    dirname=$(dirname $fn_in)
+    basename=$(basename $fn_in)
+    # base="${dirname}/${basename%.*}.png"
+    fn_out="${dirname}/${cnt}.jpg"
+    xoffset=1600
+    yoffset=800
+    width=3200
+    height=2400
+    echo "Croping $fn_in to $width*$height from ($xoffset, $yoffset): $fn_out..."
+    convert -crop ${width}x${height}+${xoffset}+${yoffset} $fn_in $fn_out
+    cnt=$(($cnt + 1))
+done
