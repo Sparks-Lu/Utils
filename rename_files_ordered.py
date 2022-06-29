f983eba65d167b487511e9af2dd4c175f0be3489
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2022-06-29 20:17:14 +0800

    Support prefix when rename orderly.

--- rename_files_ordered.py
+++ rename_files_ordered.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 
-def rename_files_ordered(path):
+def rename_files_ordered(path, prefix):
     if not os.path.isdir(path):
         raise Exception('Invalid folder name: {}'.format(path))
     fns = os.listdir(path)
@@ -11,8 +11,10 @@ def rename_files_ordered(path):
     for i, fn in enumerate(fns):
         fn_in = os.path.join(path, fn)
         # fn_tmp = '{}{}'.format(str(i + 1).zfill(id_str_width),
-        fn_tmp = '{}{}'.format(str(i + 1),
-                               os.path.splitext(fn)[-1])
+        fn_tmp = '{}{}{}'.format(
+            prefix,
+            str(i + 1),
+            os.path.splitext(fn)[-1])
         fn_out = os.path.join(path, fn_tmp)
         shutil.move(fn_in, fn_out)
         print('Renamed {} to {}'.format(fn_in, fn_out))
@@ -21,7 +23,11 @@ def rename_files_ordered(path):
 def main():
     import sys
     path = sys.argv[1]
-    rename_files_ordered(path)
+    if len(sys.argv) > 2:
+        prefix = sys.argv[2]
+    else:
+        prefix = ''
+    rename_files_ordered(path, prefix)
 
 
 if __name__ == '__main__':
