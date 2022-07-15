7ea3f5b3877d7b65308762b7bb8cc3094b970e93
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2022-07-15 12:15:20 +0800

    Updated geojson formatter.

--- geojson/geojson_formatter.sh
+++ geojson/geojson_formatter.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 fn_input=$1
 cp $fn_input ${fn_input}.bak
-sed 's/","features"/",\n"features"/g' ${fn_input}.bak > $fn_input
+sed 's/",\s*"features"/",\n"features"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/{"type"/{\n"type"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/","properties"/",\n"properties"/g' ${fn_input}.bak > $fn_input
+sed 's/{\s*"type"/{\n"type"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/},"geometry"/},\n"geometry"/g' ${fn_input}.bak > $fn_input
+sed 's/",\s*"properties"/",\n"properties"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/","coordinates"/",\n"coordinates"/g' ${fn_input}.bak > $fn_input
+sed 's/},\s*"properties"/},\n"properties"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/]}/]\n}/g' ${fn_input}.bak > $fn_input
+sed 's/},\s*"geometry"/},\n"geometry"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/}}/}\n}/g' ${fn_input}.bak > $fn_input
+sed 's/",\s*"geometry"/",\n"geometry"/g' ${fn_input}.bak > $fn_input
 cp $fn_input ${fn_input}.bak
-sed 's/},{/},\n{/g' ${fn_input}.bak > $fn_input
+sed 's/",\s*"coordinates"/",\n"coordinates"/g' ${fn_input}.bak > $fn_input
+cp $fn_input ${fn_input}.bak
+sed 's/]\s*}/]\n}/g' ${fn_input}.bak > $fn_input
+cp $fn_input ${fn_input}.bak
+sed 's/}\s*}/}\n}/g' ${fn_input}.bak > $fn_input
+cp $fn_input ${fn_input}.bak
+sed 's/},\s*{/},\n{/g' ${fn_input}.bak > $fn_input
