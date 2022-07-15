d363cf07b854d696fb634d6ad6a7b6e910aec3d8
Sparks-Lu <lgm@Laptop-Sparks.localdomain>
2022-07-15 09:12:50 +0800

    Updated image processing utils.

--- resize_images.sh
+++ resize_images.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 files=`ls $1/*.jpg`
-echo "Files to be processed: $files"
+ratio=$2
+echo "Scale ratio: $ratio, Files to be processed: $files"
 cnt=1
 for fn_in in $files; do
     # dirname=$(dirname $fn_in)
@@ -8,8 +9,14 @@ for fn_in in $files; do
     # base="${dirname}/${basename%.*}.png"
     # fn_out="${dirname}/${cnt}.jpg"
     fn_out=${fn_in}
-    width=1500
-    height=1300
+    wh=`identify -format '%w %h' ${fn_in}`
+    echo "wh: $wh"
+    width_old=$(echo $wh | cut -d' ' -f1)
+    height_old=$(echo $wh | cut -d' ' -f2)
+    echo "old width: $width_old, old height: $height_old"
+    width=$(echo "$width_old*$ratio" | bc)
+    height=$(echo "$height_old*$ratio" | bc)
+    echo "width: $width, height: $height"
     echo "Resizing $fn_in to $width*$height: $fn_out..."
     convert -resize ${width}x${height} $fn_in $fn_out
     cnt=$(($cnt + 1))
