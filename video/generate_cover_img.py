3ce615e35ac68607a76e6a4ed08758fd9f0f7f5e
Sparks-Lu <lostinfog@126.com>
2024-04-28 20:36:23 +0800

    Scaled down video cover.

--- video/generate_cover_img.py
+++ video/generate_cover_img.py
@@ -4,7 +4,7 @@ import sys
 import cv2 as cv
 
 
-def generate_cover_image_from_video(video_path, output_image_path):
+def generate_cover_image_from_video(video_path, output_image_path, max_wh):
     cap = cv.VideoCapture(video_path)
     if not cap.isOpened():
         print("Error: Could not open video.")
@@ -14,24 +14,31 @@ def generate_cover_image_from_video(video_path, output_image_path):
         print("Error: Could not read frame from video.")
         cap.release()
         return
+    height, width = frame.shape[:2]
+    if width > max_wh or height > max_wh:
+        scale_w = max_wh / width
+        scale_h = max_wh / height
+        scale = min(scale_w, scale_h)
+        frame = cv.resize(frame, (int(width * scale), int(height * scale)))
     cv.imwrite(output_image_path, frame)
     cap.release()
     print(f'Cover image saved to {output_image_path}')
 
 
-def generate_cover(input_path):
+def generate_cover(input_path, max_wh):
     for tmp_fn in os.listdir(input_path):
         if tmp_fn.endswith('.mp4') or tmp_fn.endswith('.mov'):
             base, _ = os.path.splitext(tmp_fn)
             fn_out = '{}_cover.jpg'.format(base)
             fn_full = os.path.join(input_path, tmp_fn)
             fn_out_full = os.path.join(input_path, fn_out)
-            generate_cover_image_from_video(fn_full, fn_out_full)
+            generate_cover_image_from_video(fn_full, fn_out_full, max_wh)
 
 
 def main():
     input_path = sys.argv[1]
-    generate_cover(input_path)
+    max_wh = 600
+    generate_cover(input_path, max_wh)
 
 
 if __name__ == '__main__':
