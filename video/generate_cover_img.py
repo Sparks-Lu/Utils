178a609b95a9b4da61c0eb18c190eb823d11da2d
Sparks-Lu <lostinfog@126.com>
2024-04-28 20:21:26 +0800

    Added video cover image generation.

---
+++ video/generate_cover_img.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+import cv2 as cv
+
+
+def generate_cover_image_from_video(video_path, output_image_path):
+    cap = cv.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame from video.")
+        cap.release()
+        return
+    cv.imwrite(output_image_path, frame)
+    cap.release()
+    print(f'Cover image saved to {output_image_path}')
+
+
+def generate_cover(input_path):
+    for tmp_fn in os.listdir(input_path):
+        if tmp_fn.endswith('.mp4'):
+            base, _ = os.path.splitext(tmp_fn)
+            fn_out = '{}_cover.jpg'.format(base)
+            fn_full = os.path.join(input_path, tmp_fn)
+            fn_out_full = os.path.join(input_path, fn_out)
+            generate_cover_image_from_video(fn_full, fn_out_full)
+
+
+def main():
+    input_path = sys.argv[1]
+    generate_cover(input_path)
+
+
+if __name__ == '__main__':
+    main()
