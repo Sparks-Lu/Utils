f636bf4fe0c6a192aa533b5e6d206a208ed5be32
Sparks-Lu <lostinfog@126.com>
2024-06-27 17:49:15 +0800

    Updated webp related codes.

---
+++ img/webp/extract_webp_frames.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+import webp
+
+
+def extract_webp_frames(webp_file, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    frames = webp.load_images(webp_file)
+    frame_count = len(frames)
+    print(f'Total {frame_count} frames in {webp_file}')
+    for i, frame in enumerate(frames):
+        frame_output_file = os.path.join(output_dir, f'frame_{i:03d}.png')
+        frame.save(frame_output_file)
+        print(f'Saved [{i}] frame as {frame_output_file}')
+
+
+def main():
+    webp_file = sys.argv[1]
+    output_dir = 'frames'
+    extract_webp_frames(webp_file, output_dir)
+
+
+if __name__ == '__main__':
+    main()
--- img/webp/generate_animated_webp.sh
+++ img/webp/generate_animated_webp.sh
@@ -1,14 +1,26 @@
-dir=/mnt/d/Projects/ExpoVR/2024MITTechBreakthrough/
-webpmux \
-    -frame $dir/cover.webp +3000 \
-    -frame $dir/1.webp +3000+0+0+0+b \
-    -frame $dir/2.webp +3000+0+0+0+b \
-    -frame $dir/3.webp +3000+0+0+0+b \
-    -frame $dir/4.webp +3000+0+0+0+b \
-    -frame $dir/5.webp +3000+0+0+0+b \
-    -frame $dir/6.webp +3000+0+0+0+b \
-    -frame $dir/7.webp +3000+0+0+0+b \
-    -frame $dir/8.webp +3000+0+0+0+b \
-    -frame $dir/9.webp +3000+0+0+0+b \
-    -frame $dir/10.webp +3000+0+0+0+b \
-    -o $dir/Cover1.webp
+#!/bin/bash
+
+echo "Input source image path: (filenames must follow frame_xxx.webp)"
+read input_dir
+if [ ! -d "$input_dir" ]; then
+    echo "Error: Input directory does not exist."
+    exit 1
+fi
+echo "Input pause time (ms) between frames: "
+read pause_ms
+
+output_file=$input_dir/output.webp
+webpmux_cmd="webpmux"
+for i in {000..007}; do
+    frame_file="${input_dir}/frame_${i}.webp"
+    webpmux_cmd="${webpmux_cmd} -frame $frame_file +$pause_ms"
+done
+webpmux_cmd="${webpmux_cmd} -o $output_file"
+echo "Command: $webpmux_cmd"
+eval "$webpmux_cmd"
+
+if [ $? -eq 0 ]; then
+    echo "Created output animated webp image: $output_file"
+else
+    echo "Error creating animated webp image."
+fi
