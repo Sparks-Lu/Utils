de23d48a90bc1f79d1c516cbb010b1b60baf0adc
Sparks-Lu <lostinfog@126.com>
2024-08-14 16:41:50 +0800

    Updated webp2other to support input folder.

---
+++ build_scripts/build_colmap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+    git \
+    cmake \
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-filesystem-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
+    libflann-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qtbase5-dev \
+    libqt5opengl5-dev \
+    libcgal-dev \
+    libceres-dev \
+    nvidia-cuda-toolkit \
+    nvidia-cuda-toolkit-gcc
+
+# git clone https://github.com/colmap/colmap.git
+# cd colmap
+# mkdir build
+cd build
+cmake .. -GNinja
+ninja
+sudo ninja install
--- img/webp/webp2other.py
+++ img/webp/webp2other.py
@@ -5,22 +5,37 @@ from webptools import dwebp
 
 
 def main():
-    fn_input = sys.argv[1]
-    ext1 = os.path.splitext(os.path.basename(fn_input))[-1]
-    ext1 = ext1.lower()
-    if ext1 != '.webp':
-        print('Input file must be .webp')
-        return -1
-    fn_output = sys.argv[2]
-    ext2 = os.path.splitext(os.path.basename(fn_output))[-1]
-    ext2 = ext2.lower()
-    if ext2 != '.jpg' and ext2 != '.jpeg' and ext2 != 'bmp' \
-       and ext2 != 'png' and ext2 != '.gif':
+    new_ext = sys.argv[2]
+    if new_ext not in ['.jpg', '.jpeg', '.bmp', '.png', '.gif']:
         print('Output file must be .gif/.jpg/.jpeg/.png/.bmp')
         return -2
-    print(f'Converting {fn_input} to {fn_output}...')
-    ret = dwebp(input_image=fn_input, output_image=fn_output, option="-o", logging="-v")
-    print(f'Finished ret={ret}')
+
+    input_path = sys.argv[1]
+    if os.path.isfile(input_path):
+        base, ext1 = os.path.splitext(os.path.basename(input_path))
+        ext1 = ext1.lower()
+        if ext1 != '.webp':
+            print('Input file must be .webp')
+            return -1
+        output_path = os.path.join(os.path.dirname(input_path),
+                                   f'{base}{new_ext}')
+        print(f'Converting {input_path} to {output_path}...')
+        ret = dwebp(input_image=input_path, output_image=output_path, option="-o", logging="-v")
+        print(f'Finished ret={ret}')
+    elif os.path.isdir(input_path):
+        fns = os.listdir(input_path)
+        for fname in fns:
+            full_path = os.path.join(input_path, fname)
+            if os.path.isfile(full_path):
+                base, ext = os.path.splitext(os.path.basename(fname))
+                ext = ext.lower()
+                if ext == '.webp':
+                    output_path = os.path.join(input_path, f'{base}{new_ext}')
+                    print(f'Converting {full_path} to {output_path}...')
+                    ret = dwebp(input_image=full_path,
+                                output_image=output_path,
+                                option="-o", logging="-v")
+                    print(f'Finished ret={ret}')
 
 
 if __name__ == '__main__':
