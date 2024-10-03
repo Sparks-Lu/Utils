0e0768383b5bcf095305939ed4301364d8888906
Sparks-Lu <lostinfog@126.com>
2024-10-03 16:11:09 +0800

    Updated output path of output git.

--- img/webp/webp2gif.py
+++ img/webp/webp2gif.py
@@ -1,3 +1,4 @@
+import os
 import webp
 
 
@@ -9,7 +10,9 @@ def main():
 
     # Grab a reference to the first frame, and save the entire PIL image
     # array as GIF with 70ms frames (14.286 FPS)
-    anim[0].save('output.gif',
+    base = os.path.splitext(os.path.basename(fn))[0]
+    output_path = os.path.join(os.path.dirname(fn), f'{base}.gif')
+    anim[0].save(output_path,
                  save_all=True,
                  append_images=anim[0:],
                  duration=70,
