c4bb606b2bc3bd247156ac1c2c1e37efdadda516
Sparks-Lu <lostinfog@126.com>
2024-10-10 14:36:35 +0800

    Updated duration when generate animated webp.

--- img/webp/generate_animated_webp.py
+++ img/webp/generate_animated_webp.py
@@ -1,22 +1,35 @@
 import os
 import sys
 import subprocess
+import math
 
 import numpy as np
 from PIL import Image
 import imageio
 
-def create_animated_webp(image_folder, output_file, duration_s=0.1):
+
+def create_animated_webp(image_folder, output_file, duration_ms=None, max_wh=None):
     imgs = [Image.open(os.path.join(image_folder, img))
             for img in sorted(os.listdir(image_folder))
             if img.endswith(('png', 'jpg', 'jpeg'))]
     base_width, base_height = imgs[0].size
+    if max_wh is not None and (base_width > max_wh or base_height > max_wh):
+        # resize to smaller than max_wh
+        w_ratio = max_wh / base_width
+        h_ratio = max_wh / base_height
+        ratio = min(w_ratio, h_ratio)
+        base_width = math.floor(base_width * ratio)
+        base_height = math.floor(base_height * ratio)
     for img in imgs:
         img = img.resize((base_width, base_height))
     frames = [np.array(img) for img in imgs]
     # Create the animation and save as WebP
-    imageio.mimsave(output_file, frames, duration=duration_s / 1000, loop=0)
-    print(f'Generated animated webp: {output_file}')
+    if duration_ms is None:
+        duration_ms = 3 / len(frames) * 1000
+        if duration_ms > 200:
+            duration_ms = 200
+    imageio.mimsave(output_file, frames, duration=duration_ms, loop=0)
+    print(f'Generated animated webp duration={duration_ms}: {output_file}')
 
 
 def generate_webp_using_ffmpeg(input_path):
@@ -38,8 +51,10 @@ def main():
         print('Usage: {__name__} {image_folder}')
     # generate_webp_using_ffmpeg(input_path)
     output_file = os.path.join(input_path, 'animated.webp')
-    duration_s = 0.5
-    create_animated_webp(input_path, output_file, duration_s)
+    duration_ms = None
+    if len(sys.argv) > 2:
+        duration_ms = int(sys.argv[2])
+    create_animated_webp(input_path, output_file, duration_ms)
 
 
 if __name__ == '__main__':
